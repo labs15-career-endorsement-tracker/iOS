c6f3d2bb4529bbaf2e563711fdd5d88fca1e5323
victor <ruizdvictor@gmail.com>
2019-08-16 14:26:27 -0400

    added config class for constants and created pod file

---
+++ Career Endorsement Tracker/Career Endorsement Tracker/Models/CurrentUser.swift
@@ -0,0 +1,23 @@
+//
+//  CurrentUser.swift
+//  Career Endorsement Tracker
+//
+//  Created by Victor  on 8/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+//Model for user signed in
+struct CurrentUser {
+    let uid: String
+    var name: String
+    var email: String
+    
+    init(uid: String, dictionary: [String: Any]) {
+        self.uid = uid
+        self.name = dictionary["name"] as? String ?? ""
+        self.email = dictionary["email"] as? String ?? ""
+    }
+}
---
+++ Career Endorsement Tracker/Pods/Target Support Files/Pods-Career Endorsement Tracker/Pods-Career Endorsement Tracker-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Career_Endorsement_Tracker : NSObject
+@end
+@implementation PodsDummy_Pods_Career_Endorsement_Tracker
+@end

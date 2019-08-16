3f6c726e43ee8794d36fb5b0e8273fbe3310ca8b
victor <ruizdvictor@gmail.com>
2019-08-16 14:14:12 -0400

    added fetching functionality

---
+++ Career Endorsement Tracker/Career Endorsement Tracker/Networking /DataGetter.swift
@@ -0,0 +1,43 @@
+//
+//  DataGetter.swift
+//  Career Endorsement Tracker
+//
+//  Created by Victor  on 8/16/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class DataGetter {
+    //setting constants
+    enum HTTPError: Error {
+        case non200StatusCode
+        case noData
+    }
+    
+    //retrieves data
+    func fetchData(with request: URLRequest, requestID: String? = nil, completion: @escaping (String?, Data?, Error?) -> Void) {
+        
+        //data task
+        URLSession.shared.dataTask(with: request) { (data, response, error) in
+            //error handling
+            if let error = error {
+                print(error)
+                completion(requestID, nil, error)
+                return
+            } else if let response = response as? HTTPURLResponse, response.statusCode != 200 {
+                print("non 200 http response: \(response.statusCode)")
+                let myError = HTTPError.non200StatusCode
+                completion(requestID, nil, myError)
+                return
+            }
+            
+            guard let data = data else {
+                completion(requestID, nil, HTTPError.noData)
+                return
+            }
+            completion(requestID, data, nil)
+            }.resume()
+    }
+}

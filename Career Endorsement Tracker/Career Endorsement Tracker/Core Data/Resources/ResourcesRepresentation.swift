3ec7d9391cec533ac198dbf48f32d3b704268f1d
Alex <alexcc17@gmail.com>
2019-08-27 12:16:04 -0400

    Create Core Data Representation and Convenience classes for all entities.

---
+++ Career Endorsement Tracker/Career Endorsement Tracker/Core Data/Resources/ResourcesRepresentation.swift
@@ -0,0 +1,9 @@
+//
+//  ResourcesRepresentation.swift
+//  Career Endorsement Tracker
+//
+//  Created by Alex on 8/27/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
---
+++ Career Endorsement Tracker/Career Endorsement Tracker/Core Data/Tasks_tracks/Task_TracksRepresentation.swift
@@ -0,0 +1,9 @@
+//
+//  Task_TracksRepresentation.swift
+//  Career Endorsement Tracker
+//
+//  Created by Alex on 8/27/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation

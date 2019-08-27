950aad0b3ef05d12d0b27fe1e6fa9bd1618966c9
victor <ruizdvictor@gmail.com>
2019-08-27 17:07:38 -0400

    updated sign up flow logic and views

--- Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Welcome/LoginViewController.swift
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Welcome/LoginViewController.swift
@@ -24,18 +24,21 @@ class LoginViewContoller: UIViewController {
     func login() {
         guard let username = emailTextField.text, !username.isEmpty,
             let password = passwordTextField.text, !password.isEmpty
-            else { return }
+            else {
+                Config.showAlert(on: self, style: .alert, title: "Login Error", message: "Please make sure all fields are completed.")
+                return
+        }
         let user = CurrentUser(username: username, password: password)
         
-        server.loginWith(user: user, completion: { [weak self] (error) in
+        server.loginWith(user: user, completion: { (error) in
             if let error = error {
-                Config.showAlert(on: self!, style: .alert, title: "Login Error", message: error.localizedDescription)
+                Config.showAlert(on: self, style: .alert, title: "Login Error", message: error.localizedDescription)
                 return
             } else {
                 DispatchQueue.main.async {
                     let storyboard = UIStoryboard(name: "Main", bundle: nil)
                     let vc = storyboard.instantiateViewController(withIdentifier: "MainTabBarController")
-                    self!.present(vc, animated: true, completion: nil)
+                    self.present(vc, animated: true, completion: nil)
                 }
             }
         })
--- Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Welcome/SignUpViewController.swift
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Welcome/SignUpViewController.swift
@@ -10,10 +10,73 @@ import Foundation
 import UIKit
 
 class SignUpViewController: UIViewController {
+    
+    let server = Server()
+    
+    @IBOutlet weak var nameTextField: UITextField!
+    @IBOutlet weak var trackTextField: UITextField!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var registerButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateViews()
     }
+    
+    func signUp() {
+        
+        guard let name = nameTextField.text, !name.isEmpty, let track = trackTextField.text, !track.isEmpty, let username = emailTextField.text, !username.isEmpty, let password = passwordTextField.text, !password.isEmpty else {
+            Config.showAlert(on: self, style: .alert, title: "SignUp Error", message: "Please make sure all fields are completed.")
+            return
+        }
+        
+        
+        let user = CurrentUser(username: username, password: password)
+        
+        server.loginWith(user: user) { (error) in
+            if let error = error  {
+                Config.showAlert(on: self, style: .alert, title: "Sign Up Error", message: error.localizedDescription)
+                return
+            } else {
+                DispatchQueue.main.async {
+                    let storyboard = UIStoryboard(name: "Main", bundle: nil)
+                    let vc = storyboard.instantiateViewController(withIdentifier: "MainTabBarController")
+                    self.present(vc, animated: true, completion: nil)
+                }
+            }
+            
+        }
+        
+    }
+    
+    
+    @IBAction func registerButtonPressed(_ sender: Any) {
+        signUp()
+    }
+    
+    func updateViews() {
+        registerButton.layer.masksToBounds = true
+        registerButton.layer.cornerRadius = Config.buttonCornerRadius
+    }
+    
+    
+    //MARK: Validation
+    
+    //name check
+    func isValidName(testStr:String) -> Bool {
+        let nameRegEx = "^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$"
+        let nameTest = NSPredicate(format:"SELF MATCHES %@", nameRegEx)
+        return nameTest.evaluate(with: testStr)
+    }
+    
+    //email check
+    func isValidEmail(testStr:String) -> Bool {
+        let emailRegEx = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,64}"
+        
+        let emailTest = NSPredicate(format:"SELF MATCHES %@", emailRegEx)
+        return emailTest.evaluate(with: testStr)
+    }
+    
+    
 }

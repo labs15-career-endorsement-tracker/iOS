a37aece9fc0796c980b12015ac7836054ebb79aa
Alex <alexcc17@gmail.com>
2019-09-04 22:23:06 -0400

    Create Steps cell.

---
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Steps/StepsViewController.swift
@@ -0,0 +1,54 @@
+//
+//  StepsViewController.swift
+//  Career Endorsement Tracker
+//
+//  Created by Alex on 9/4/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class StepsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
+
+extension StepsViewController: UICollectionViewDelegate {
+    
+}
+extension StepsViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "steps_cell", for: indexPath) as! StepsCollectionViewCell
+
+        return cell
+    }
+}
+
+extension StepsViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return  CGSize(width: collectionView.bounds.size.width, height: 80)
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 5
+    }
+}

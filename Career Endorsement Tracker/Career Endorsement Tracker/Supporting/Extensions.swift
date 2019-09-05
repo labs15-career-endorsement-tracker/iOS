3cf8a72507e2c6ffe865fb20b1a5a3063688c9ef
Alex <alexcc17@gmail.com>
2019-09-05 12:31:39 -0400

    Added extension on uiview to display a shadow background.

--- Career Endorsement Tracker/Career Endorsement Tracker/Supporting/Extensions.swift
+++ Career Endorsement Tracker/Career Endorsement Tracker/Supporting/Extensions.swift
@@ -9,6 +9,11 @@
 import Foundation
 import UIKit
 
+struct UIViewExtensionConstants {
+    static let CornerRadiusForCell : CGFloat = 25
+    static let CornerRadiusForLabel : CGFloat = 20
+}
+
 extension UITextField {
     func underlined(color: UIColor){
         var border = CALayer()
@@ -18,5 +23,17 @@ extension UITextField {
         border.borderWidth = width
         self.layer.addSublayer(border)
         self.layer.masksToBounds = true
+    }
+}
+
+extension UIView {
+    func shadowCell(){
+        self.layer.cornerRadius = UIViewExtensionConstants.CornerRadiusForCell
+        self.layer.masksToBounds = true
+        self.layer.shadowRadius = 5.0
+        self.layer.shadowColor = UIColor(red:0.37, green:0.37, blue:0.37, alpha:1).cgColor
+        self.layer.shadowOpacity = 0.34
+        self.layer.shadowOffset = CGSize(width: 2, height: 5)
+        self.layer.masksToBounds = false
     }
 }

1e3160410357d7bc9aa5a3b37a19f98865546d04
Sameera Leola <pixelandbyte@gmail.com>
2019-09-05 18:44:21 -0500

    Styled Welcome screens

--- Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Welcome/WelcomeViewController.swift
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Welcome/WelcomeViewController.swift
@@ -16,12 +16,27 @@ class WelcomeViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
         updateViews()
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        self.navigationController?.navigationBar.isHidden = false;
+    }
+    
     func updateViews() {
+        self.navigationController?.navigationBar.isHidden = true;
+        self.navigationController?.navigationBar.tintColor = Config.buttonTitleColor
+       //
         loginButton.layer.masksToBounds = true
         loginButton.layer.cornerRadius = Config.buttonCornerRadius
+        loginButton.layer.borderWidth = 1
+        loginButton.layer.borderColor = Config.themeGreen.cgColor
+       //
+        signUpButton.layer.borderWidth = 2
+        signUpButton.layer.borderColor = UIColor.black.cgColor
         signUpButton.layer.masksToBounds = true
         signUpButton.layer.cornerRadius = Config.buttonCornerRadius
     }

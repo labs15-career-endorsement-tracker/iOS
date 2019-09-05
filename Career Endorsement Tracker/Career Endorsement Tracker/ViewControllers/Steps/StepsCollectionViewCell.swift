4cc8a5406a5486d95ae7e602c891e35385f90b19
Alex <alexcc17@gmail.com>
2019-09-05 19:31:50 -0400

    Add storyboard files

---
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Steps/StepsCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  StepsCollectionViewCell.swift
+//  Career Endorsement Tracker
+//
+//  Created by Alex on 9/5/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class StepsCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Steps/StepsViewController.swift
@@ -0,0 +1,30 @@
+//
+//  StepsViewController.swift
+//  Career Endorsement Tracker
+//
+//  Created by Alex on 9/5/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class StepsViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

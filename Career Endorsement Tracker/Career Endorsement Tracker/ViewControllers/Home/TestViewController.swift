d00fa44e853cf69ae0794f6c3a188581a610b32f
victor <ruizdvictor@gmail.com>
2019-09-10 19:11:14 -0400

    updated fonts for header view

--- Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Home/TestViewController.swift
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Home/TestViewController.swift
@@ -20,16 +20,39 @@ class TestViewController: UIViewController {
     }
     
     func updateViews() {
-        self.welcomeView.layer.shadowPath =
-            UIBezierPath(roundedRect: self.welcomeView.bounds,
-                         cornerRadius: self.welcomeView.layer.cornerRadius).cgPath
-        self.welcomeView.layer.shadowColor = UIColor.black.cgColor
-        self.welcomeView.layer.shadowOpacity = 0.5
-        self.welcomeView.layer.shadowOffset = CGSize(width: 10, height: 10)
-        self.welcomeView.layer.shadowRadius = 1
-        self.welcomeView.layer.masksToBounds = false
+        welcomeView.dropShadow()
     }
     
+}
+
+
+extension UIView {
+    
+    // OUTPUT 1
+    func dropShadow(scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOpacity = 0.5
+        layer.shadowOffset = CGSize(width: -1, height: 1)
+        layer.shadowRadius = 1
+        
+        layer.shadowPath = UIBezierPath(rect: bounds).cgPath
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
+    
+    // OUTPUT 2
+    func dropShadow(color: UIColor, opacity: Float = 0.5, offSet: CGSize, radius: CGFloat = 1, scale: Bool = true) {
+        layer.masksToBounds = false
+        layer.shadowColor = color.cgColor
+        layer.shadowOpacity = opacity
+        layer.shadowOffset = offSet
+        layer.shadowRadius = radius
+        
+        layer.shadowPath = UIBezierPath(rect: self.bounds).cgPath
+        layer.shouldRasterize = true
+        layer.rasterizationScale = scale ? UIScreen.main.scale : 1
+    }
 }
 
 

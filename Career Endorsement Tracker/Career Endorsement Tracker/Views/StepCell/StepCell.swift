c989167388d6bdfc5736b0fdbd03aa1609a976d8
Miikis <28997062+Miikis@users.noreply.github.com>
2019-09-10 22:06:00 -0700

    Add StepCell and nib
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

--- Career Endorsement Tracker/Career Endorsement Tracker/Views/StepCell/StepCell.swift
+++ Career Endorsement Tracker/Career Endorsement Tracker/Views/StepCell/StepCell.swift
@@ -8,17 +8,91 @@
 
 import UIKit
 
-class StepCell: UITableViewCell {
+protocol StepData {
+    var is_complete: Bool { get }
+    var steps_description: String { get }
+}
 
+extension Step: StepData {}
+
+protocol StepCellDelegate {
+    func didSelect(_ cell: StepCell, atIndexPath indexPath: IndexPath)
+}
+
+class StepCell: UITableViewCell {
+    @IBOutlet weak var cellBackgroundView: UIView!
+    @IBOutlet weak var shadowView: ShadowView!
+    @IBOutlet weak var stepTextView: UITextView!
+    @IBOutlet weak var checkmarkView: UIImageView!
+    
+    var step: StepData?
+    var indexPath: IndexPath!
+    var delegate: StepCellDelegate?
+    
+    static let reuseId: String = "StepTableViewCell"
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+        
+        updateTextView()
     }
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        setupCard()
+        updateTextView()
+        updateCheckmark()
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        updateCheckmark()
+        delegate?.didSelect(self, atIndexPath: indexPath)
+    }
+}
+
+extension StepCell {
+    fileprivate func setupCard() {
+        selectionStyle = .none
+        contentView.backgroundColor = .clear
+        backgroundColor = .clear
+        cellBackgroundView.layer.masksToBounds = true
+    }
+    
+    fileprivate func updateTextView() {
+        guard let step = step else {
+            print("Warning: No step data attached to StepCell!")
+            return
+        }
+        
+        stepTextView.text = step.steps_description
+        
+        let size = CGSize(width: cellBackgroundView.frame.width, height: .infinity)
+        let estimatedSize = stepTextView.sizeThatFits(size)
+        
+        stepTextView.translatesAutoresizingMaskIntoConstraints = false
+        stepTextView.constraints.forEach { (constraint) in
+            if constraint.firstAttribute == .height {
+                constraint.constant = estimatedSize.height
+            }
+        }
+    }
+    
+    fileprivate func updateCheckmark() {
+        guard let step = step else {
+            print("Warning: No step data attached to StepCell!")
+            return
+        }
+        
+        guard step.is_complete else {
+            checkmarkView.image = nil
+            checkmarkView.layer.borderWidth = 4
+            checkmarkView.layer.borderColor = UIColor(red: 0.34, green: 0.93, blue: 0.46, alpha: 1).cgColor
+            return
+        }
+        
+        checkmarkView.image = UIImage(imageLiteralResourceName: "checkbox")
+        checkmarkView.layer.borderWidth = 0
+        checkmarkView.layer.borderColor = UIColor.clear.cgColor
+    }
 }

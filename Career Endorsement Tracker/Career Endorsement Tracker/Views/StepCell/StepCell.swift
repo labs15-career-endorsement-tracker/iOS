4a5a96cddb4283add6315dfb74cb280a3fbf4596
Miikis <28997062+Miikis@users.noreply.github.com>
2019-09-10 22:00:19 -0700

    Update cocoapods to 1.7.5
    Signed-off-by: Miikis <28997062+Miikis@users.noreply.github.com>

---
+++ Career Endorsement Tracker/Career Endorsement Tracker/Views/StepCell/StepCell.swift
@@ -0,0 +1,24 @@
+//
+//  StepCell.swift
+//  Career Endorsement Tracker
+//
+//  Created by Mikis Woodwinter on 9/10/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class StepCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}

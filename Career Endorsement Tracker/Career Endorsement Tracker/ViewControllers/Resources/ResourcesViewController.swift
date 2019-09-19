9a8d8054b723aeae2866fabc2d3cfc51b7b8888e
Alex <alexcc17@gmail.com>
2019-09-19 14:32:54 -0400

    Deselect cell

--- Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Resources/ResourcesViewController.swift
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Resources/ResourcesViewController.swift
@@ -52,5 +52,9 @@ extension ResourcesViewController: UITableViewDataSource {
         return cell
     }
     
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
     
 }

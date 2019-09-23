524d82fe3d8d824475e6772f65bdbfea2a0c2971
Sameera Leola <pixelandbyte@gmail.com>
2019-09-23 16:41:22 -0500

    Added Forgot Password view to Welcome storyboard

---
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Signin/RequestPasswordResetViewController.swift
@@ -0,0 +1,52 @@
+//
+//  RequestPasswordResetViewController.swift
+//  Career Endorsement Tracker
+//
+//  Created by Sameera Leola on 9/23/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class RequestPasswordResetViewController: UIViewController {
+    
+    // MARK: - Properties
+    
+    
+    // MARK: - Outlets
+    @IBOutlet weak var emailAddressField: UITextField!
+    
+    
+    // MARK: - View states
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+    
+    // MARK: - Actions
+    @IBAction func requestPasswordTapped(_ sender: Any) {
+    }
+    
+    @IBAction func cancelRequestTapped(_ sender: Any) {
+    }
+    
+    
+    // MARK: - Functions
+    
+    
+
+
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

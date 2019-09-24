93826a9a440102bd98500d8da57bd4ee106b7425
Sameera Leola <pixelandbyte@gmail.com>
2019-09-24 00:21:00 -0500

    Updated UI on Forgot Password scene Added segue from Log in scene to request password reset scene which populates email address field on the reset request scene. Implemented Cancel request in RequestPasswordResetViewController to dismiss reset request back to Login scene. Added updateAfterEmail func to populate the email field with email address passed in segue.

--- Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Signin/RequestPasswordResetViewController.swift
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/Signin/RequestPasswordResetViewController.swift
@@ -11,7 +11,8 @@ import UIKit
 class RequestPasswordResetViewController: UIViewController {
     
     // MARK: - Properties
-    
+    let server = Server()
+    var emailAddress: String?
     
     // MARK: - Outlets
     @IBOutlet weak var emailAddressField: UITextField!
@@ -20,20 +21,30 @@ class RequestPasswordResetViewController: UIViewController {
     // MARK: - View states
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
         
-        // Do any additional setup after loading the view.
+        // Make sure we received an email address
+        updateAfterEmail()
     }
     
     // MARK: - Actions
     @IBAction func requestPasswordTapped(_ sender: Any) {
     }
     
+    
     @IBAction func cancelRequestTapped(_ sender: Any) {
+        self.dismiss(animated: true, completion: nil)
     }
     
     
     // MARK: - Functions
-    
+    func updateAfterEmail() {
+        guard let emailAddress = emailAddress  else { return }
+        emailAddressField.text = emailAddress
+    }
     
 
 

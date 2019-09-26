e7a649e15e3f9534d30f61e65e25d2c5fdd46b33
Alex <alexcc17@gmail.com>
2019-09-26 13:25:40 -0400

    Add progress bar

--- Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/CoachSearch/SearchDetailTableViewController.swift
+++ Career Endorsement Tracker/Career Endorsement Tracker/ViewControllers/CoachSearch/SearchDetailTableViewController.swift
@@ -6,17 +6,24 @@
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
-import Foundation
 import UIKit
+import UICircularProgressRing
 
 class SearchDetailTableViewController: UITableViewController {
 
+    // MARK: - Properties
+    
     var studentId: Int?
     let server = Server()
     var student: CurrentUser?
     var requirements: [Requirement] = []
     
+    // MARK: - Outlets
+    
+    @IBOutlet weak var progressBar: UICircularProgressRing!
     @IBOutlet weak var progressLabel: UILabel!
+    
+    // MARK: - VC Lifecycle
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -25,30 +32,73 @@ class SearchDetailTableViewController: UITableViewController {
         updateViews()
     }
     
+    // MARK: - Setup UI
+    
     func updateViews() {
         guard let student = student else {
             return
         }
         title = student.first_name + " " + student.last_name
+        
+        progressBar.maxValue = 100
+        progressBar.style = .dashed(pattern: [1.0, 1.0])
+        progressBar.innerRingColor = Config.lightGreenDesignColor
+        progressBar.fontColor = .white
     }
+    
+    private func updateProgress(progress: Int) {
+        progressBar.startProgress(to: CGFloat(progress), duration: 2.0) {
+            print("Done animating!")
+        }
+    }
+    
+    // MARK: - TableView
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return requirements.count
     }
     
-     
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell", for: indexPath)
+        
+        // Get the student
+        let requirement = requirements[indexPath.row]
+        
+        cell.textLabel?.text = requirement.title
+        print("HERE progress: ", requirement.progress)
+        updateProgress(progress: requirement.progress)
+//        cell.detailTextLabel?.isHidden = true
+//        cell.detailTextLabel?.text = "\(requirement.progress)"
+        
+        return cell
+    }
     
+    // MARK: - Fetch
+    
+    func fetchRequirements() {
+        guard let token = UserDefaults.standard.string(forKey: "token") else {return print("no token")}
+        guard let studentId = studentId else {return print("no id")}
+        
+        server.fetchUserRequirements(withToken: token, userID: studentId) { (requirements, error) in
+            if let error = error {
+                DispatchQueue.main.async {
+                    Config.showAlert(on: self, style: .alert, title: "Error fetching requirements", message: error.localizedDescription)
+                    return
+                }
+            }
+            
+            if let requirements = requirements {
+                self.requirements = requirements
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+            }
+        }
+    }
+
     func fetchUser() {
-        
-        guard let token = UserDefaults.standard.string(forKey: "token") else {
-            print("no token")
-            return
-        }
-        
-        guard let studentId = studentId else {
-            print("no id")
-            return
-        }
+        guard let token = UserDefaults.standard.string(forKey: "token") else {return print("no token")}
+        guard let studentId = studentId else {return print("no id")}
         
         server.fetchUser(withId: token, withUserId: studentId) { (student, error) in
             if let error = error {
@@ -67,49 +117,5 @@ class SearchDetailTableViewController: UITableViewController {
             }
         }
     }
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "StudentCell", for: indexPath)
-        
-        // Get the student
-        let requirement = requirements[indexPath.row]
-        
-        cell.textLabel?.text = requirement.title
-        cell.detailTextLabel?.text = "\(requirement.progress)"
-        
-        return cell
-    }
-    
-    func fetchRequirements() {
-        guard let token = UserDefaults.standard.string(forKey: "token") else {
-            print("no token")
-            return
-        }
-        
-        guard let studentId = studentId else {
-            print("no id")
-            return
-        }
-        
-        server.fetchUserRequirements(withToken: token, userID: studentId) { (requirements, error) in
-            if let error = error {
-                DispatchQueue.main.async {
-                    Config.showAlert(on: self, style: .alert, title: "Error fetching requirements", message: error.localizedDescription)
-                    return
-                }
-            }
-            
-            if let requirements = requirements {
-                self.requirements = requirements
-                
-                DispatchQueue.main.async {
-                    
-                    self.tableView.reloadData()
-                }
-                
-            }
-        }
-    }
-
     
 }
